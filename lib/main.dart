d7a08aa55ed8b912f33d1c4478b567113d9d52a4
Ofir MIRKIN <60558136+ofirmirkin@users.noreply.github.com>
2023-02-17 16:52:56 +0000

    Added some kind of get current location

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:location/location.dart';
 
 void main() => runApp(const MyApp());
 
@@ -13,10 +14,24 @@ class MyApp extends StatefulWidget {
 class _MyAppState extends State<MyApp> {
   late GoogleMapController mapController;
 
-  final LatLng _center = const LatLng(45.521563, -122.677433);
+  // final LatLng _center = const LatLng(45.521563, -122.677433);
+  // void _onMapCreated(GoogleMapController controller) {
+  //   mapController = controller;
+  // }
 
-  void _onMapCreated(GoogleMapController controller) {
-    mapController = controller;
+  LatLng _center = LatLng(20.5937, 78.9629);
+  late GoogleMapController _controller;
+  Location _location = Location();
+
+  void _onMapCreated(GoogleMapController _cntlr) {
+    _controller = _cntlr;
+    _location.onLocationChanged.listen((l) {
+      _controller.animateCamera(
+        CameraUpdate.newCameraPosition(
+          CameraPosition(target: LatLng(l.latitude!, l.longitude!), zoom: 15),
+        ),
+      );
+    });
   }
 
   @override
@@ -32,7 +47,7 @@ class _MyAppState extends State<MyApp> {
           elevation: 2,
         ),
         body: GoogleMap(
-          myLocationEnabled: true,
+          // myLocationEnabled: true,
           onMapCreated: _onMapCreated,
           initialCameraPosition: CameraPosition(
             target: _center,

8f8ae3e97dc45994aef19a8ae10f1c9c2705e819
Ofir MIRKIN <60558136+ofirmirkin@users.noreply.github.com>
2023-02-19 14:13:56 +0000

    Removed a few old vars and functions

--- lib/main.dart
+++ lib/main.dart
@@ -21,29 +21,13 @@ class MapSample extends StatefulWidget {
 }
 
 class MapSampleState extends State<MapSample> {
-  // Completer<GoogleMapController> _controller = Completer();
   late GoogleMapController _controller;
-
-  // Example marker
-  static const Marker _kGooglePlexMarker = Marker(
-    markerId: MarkerId('_kGooglePlex'),
-    infoWindow: InfoWindow(title: 'google plex example'),
-    icon: BitmapDescriptor.defaultMarker,
-    position: LatLng(37.42796133580664, -122.085749655962),
-  );
 
   // Initial position of the map
   static const initialCameraPosition = CameraPosition(
     target: LatLng(53.343973854161774, -6.254634551749251),
     zoom: 16,
   );
-
-  // Example position of the map
-  static const CameraPosition _kLake = CameraPosition(
-      bearing: 192.8334901395799,
-      target: LatLng(37.43296265331129, -122.08832357078792),
-      // tilt: 59.440717697143555,
-      zoom: 19.151926040649414);
 
   @override
   Widget build(BuildContext context) {
@@ -53,7 +37,6 @@ class MapSampleState extends State<MapSample> {
           mapType: MapType.normal,
           myLocationEnabled: true,
           myLocationButtonEnabled: true,
-          // markers: {_kGooglePlexMarker},
           initialCameraPosition: initialCameraPosition,
           onMapCreated: (GoogleMapController controller) {
             _controller = controller;
@@ -75,11 +58,6 @@ class MapSampleState extends State<MapSample> {
   //   Navigator.of(context)
   //       .push(MaterialPageRoute(builder: (context) => GetLocation()));
   // }
-
-  Future<void> _goToTheLake() async {
-    final GoogleMapController controller = await _controller;
-    controller.animateCamera(CameraUpdate.newCameraPosition(_kLake));
-  }
 
   Future<Position> _determinePosition() async {
     bool serviceEnabled;

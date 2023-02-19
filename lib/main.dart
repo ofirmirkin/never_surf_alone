72040bd441c1ad74a9b8600e038d3af4ea015c27
Ofir MIRKIN <60558136+ofirmirkin@users.noreply.github.com>
2023-02-19 12:52:19 +0000

    Changed a few 'final' variables to 'const'

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
 import 'dart:async';
-
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 
@@ -21,28 +20,28 @@ class MapSample extends StatefulWidget {
 }
 
 class MapSampleState extends State<MapSample> {
-  Completer<GoogleMapController> _controller = Completer();
-  static final Marker _kGooglePlexMarker = Marker(
+  final Completer<GoogleMapController> _controller = Completer();
+  static const Marker _kGooglePlexMarker = Marker(
     markerId: MarkerId('_kGooglePlex'),
     infoWindow: InfoWindow(title: 'google plex example'),
     icon: BitmapDescriptor.defaultMarker,
     position: LatLng(37.42796133580664, -122.085749655962),
   );
 
-  static final CameraPosition _kGooglePlex = CameraPosition(
+  static const CameraPosition _kGooglePlex = CameraPosition(
     target: LatLng(37.42796133580664, -122.085749655962),
     zoom: 14.4746,
   );
 
-  static final CameraPosition _kLake = CameraPosition(
+  static const CameraPosition _kLake = CameraPosition(
       bearing: 192.8334901395799,
       target: LatLng(37.43296265331129, -122.08832357078792),
-      tilt: 59.440717697143555,
+      // tilt: 59.440717697143555,
       zoom: 19.151926040649414);
 
   @override
   Widget build(BuildContext context) {
-    return new Scaffold(
+    return Scaffold(
       body: GoogleMap(
         mapType: MapType.normal,
         markers: {_kGooglePlexMarker},
@@ -53,8 +52,8 @@ class MapSampleState extends State<MapSample> {
       ),
       floatingActionButton: FloatingActionButton.extended(
         onPressed: _goToTheLake,
-        label: Text('To the lake!'),
-        icon: Icon(Icons.directions_boat),
+        label: const Text('To the lake!'),
+        icon: const Icon(Icons.hotel),
       ),
     );
   }
